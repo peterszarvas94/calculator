78acce0c778a18506bc0de0d7b52c4ecab8340cd
Szarvas Péter <peterszarvas94@gmail.com>
2023-04-21 11:17:53 +0200

    fix: dev packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,7 +41,6 @@ function App() {
   }
 
   if (mutationSuccess) {
-    console.log("mutation data", mutationSuccess);
     queryClient.invalidateQueries(['memory']);
   }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
     <React.StrictMode>
       <App />
     </React.StrictMode>
